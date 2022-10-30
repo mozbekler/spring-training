dc7253a4b6e427e13d29683a7ae550c3a517c8d0
mozbekler <104729951+mozbekler@users.noreply.github.com>
2022-10-30 18:54:43 +0000

    Some derived query methods were added to the CourseRepository

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -0,0 +1,38 @@
+package com.cydeo.repository;
+
+import com.cydeo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    //Find all courses by category
+    List<Course> findByCategory(String category);
+
+    //Find all courses by category and order entities by name
+
+    List<Course> findByCategoryOrderByName(String category);
+
+    //Checks if a course supplied name exists. Return true if exists
+
+    boolean existsByName(String name);
+
+    //Returns the count of courses for the given category
+
+    Integer countByCategory(String category);
+
+    //Find all the courses that start with the given name
+
+    List<Course> findByNameStartsWith(String name);
+
+    //Find all courses by category and returns a stream
+
+    Stream<Course> streamAllByCategory(String category);
+
+
+
+
+}

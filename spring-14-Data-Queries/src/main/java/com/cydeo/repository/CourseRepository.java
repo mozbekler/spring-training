545572ef6e07be3f7a7857d4a4aa8b0b0a2be62b
mozbekler <104729951+mozbekler@users.noreply.github.com>
2022-10-30 20:15:51 +0000

    Some JPQL query methods were added to the EmployeeRepository and DepartmentRepository

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -31,6 +32,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     //Find all courses by category and returns a stream
 
     Stream<Course> streamAllByCategory(String category);
+
+    @Query("SELECT c from Course c WHERE c.category=:category AND c.rating >:rating")
+    List<Course>  retrieveAllByCategoryAndRatingGreaterThan(@Param("category")String category, @Param("rating")int rating);
 
 
 
